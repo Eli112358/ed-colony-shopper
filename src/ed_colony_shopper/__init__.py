33a02c2ca7180192599218c22bce0670a8d8a53d
Eli112358 <4665184+Eli112358@users.noreply.github.com>
2026-04-14 00:26:25 -0400

    Search `Inara.cz` for most needed commodity

--- src/ed_colony_shopper/__init__.py
+++ src/ed_colony_shopper/__init__.py
@@ -1,11 +1,27 @@
+from datetime import timedelta
 from logging import getLogger, basicConfig, INFO
 
 from ed_colony_shopper.needed import collect_needed_commodities
+from ed_colony_shopper.search import search_inara, Market
 
 
 def main():
     logger = getLogger("ed_colony_shopper")
     basicConfig(level=INFO, format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s")
-    # logger.setLevel(logging.INFO)
     logger.info("Starting ed_colony_shopper ...")
     system, needed_commodities = collect_needed_commodities()
+    needed_most: tuple[str, int] = '', 0
+    for commodity in needed_commodities:
+        if needed_commodities[commodity][0] > needed_most[1]:
+            needed_most = commodity, needed_commodities[commodity][0]
+    markets: list[Market] = search_inara(system, needed_most[0], strict_star_dist=True)
+    oldest_market = Market("", "", 0, 0.0, 0, 0, timedelta(0))
+    cheapest_market = Market("", "", 0, 0.0, 0, 1_000_000_000, timedelta(0))
+    for market in markets:
+        if market.price < cheapest_market.price:
+            cheapest_market = market
+        if market.age > oldest_market.age:
+            oldest_market = market
+    logger.info(f"Oldest market: {oldest_market}")
+    logger.info(f"Cheapest market: {cheapest_market}")
+
---
+++ src/ed_colony_shopper/search.py
@@ -0,0 +1,74 @@
+# todo: get from configurable Google Sheet (by DaftMav)
+import logging
+from dataclasses import dataclass
+from datetime import timedelta
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from ed_colony_shopper.inara import MAPPING, URL
+
+MAX_STAR_DISTANCE: int = 30
+MAX_STATION_DISTANCE: int = 2000
+MIN_SUPPLY: int = 1000
+
+# todo: make configurable/optional
+MAX_PRICE_AGE: int = 7 * 24 # in hours
+
+
+@dataclass
+class Market:
+    station: str
+    system: str
+    station_distance: int
+    system_distance: float
+    supply: int
+    price: int
+    age: timedelta
+
+def parse_age(time_str: str) -> timedelta:
+    days, hours, minutes = 0, 0, 0
+    parts: list[str] = time_str.split(" ")
+    if parts[1].startswith("day"):
+        days = int(parts[0])
+    elif parts[1].startswith("hour"):
+        hours = int(parts[0])
+    elif parts[1].startswith("minute"):
+        minutes = int(parts[0])
+    age: timedelta = timedelta(days=days, hours=hours, minutes=minutes)
+    return age
+
+def search_inara(system: str, commodity: str, restrict_age: bool = False, strict_star_dist: bool = False) -> list[Market]:
+    logger = logging.getLogger("Inara")
+    commodity_id: int = MAPPING[commodity]
+    inara_url = URL.format(
+        commodity_id=commodity_id,
+        max_star_dist=MAX_STAR_DISTANCE,
+        max_station_dist=MAX_STATION_DISTANCE,
+        min_supply=MIN_SUPPLY,
+        max_age=MAX_PRICE_AGE if restrict_age else 0,
+        system=system.replace(" ", "+"),
+    )
+    logger.info(f"Inara URL: {inara_url}")
+    logger.info(f"Searching Inara for {commodity} near {system}...")
+    driver = webdriver.Chrome()
+    driver.get(inara_url)
+    data_rows = driver.find_elements(By.CSS_SELECTOR, "#DataTables_Table_0_wrapper tbody tr")
+    cell_data: list[list[str]] = [[cell.text for cell in row.find_elements(By.CSS_SELECTOR, "td")] for row in data_rows]
+    driver.quit()
+    results = []
+    for row in cell_data:
+        supply = row[4].split("\n")
+        supply.reverse()
+        entry = Market(
+            station=row[0].split(" | ")[0],
+            system=row[0].split(" | ")[1].split("\ue81d︎")[0],
+            station_distance=int(row[2].split(" ")[0].replace(",", "")),
+            system_distance=float(row[3].split(" ")[0]),
+            supply=int(supply[0].replace(",", "")),
+            price=int(row[5].split(" ")[0].replace(",", "")),
+            age=parse_age(row[6]),
+        )
+        if not strict_star_dist or entry.system_distance <= MAX_STAR_DISTANCE:
+            results.append(entry)
+    return results
