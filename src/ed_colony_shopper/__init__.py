1c1c2146c949f24253b89d5bb7bbde0e234f10cd
Eli112358 <4665184+Eli112358@users.noreply.github.com>
2026-04-27 01:00:31 -0400

    Refactor determining commodity needed most to function Use `lambda` as `key` in `sort` function of `list`

--- src/ed_colony_shopper/__init__.py
+++ src/ed_colony_shopper/__init__.py
@@ -12,15 +12,22 @@ def init_logger() -> Logger:
     return getLogger("E:D Colony Shopper")
 
 
+def get_system_and_commodity(logger: Logger) -> tuple[str, Commodity]:
+    system, needed_commodities = collect_needed_commodities()
+    sorted_commodities: list[Commodity] = list(needed_commodities.values())
+    sorted_commodities.sort(key=lambda c: c.needed, reverse=True)
+    logger.debug("Sorted commodities:")
+    for commodity in sorted_commodities:
+        logger.debug(f"Commodity: {commodity.name}, {commodity.needed}")
+    commodity_needed_most: Commodity = sorted_commodities[0]
+    return system, commodity_needed_most
+
+
 def main():
     logger = init_logger()
     logger.info("Starting ...")
-    system, needed_commodities = collect_needed_commodities()
-    needed_most: tuple[str, Commodity] = '', Commodity('', 0, 0, 0, 0)
-    for commodity in needed_commodities:
-        if needed_commodities[commodity].needed > needed_most[1].needed:
-            needed_most = commodity, needed_commodities[commodity]
-    markets: list[Market] = search_inara(system, needed_most[0], strict_star_dist=True)
+    system, commodity_needed_most = get_system_and_commodity(logger)
+    markets: list[Market] = search_inara(system, commodity_needed_most.name, strict_star_dist=True)
     oldest_market = Market("", "", 0, 0.0, 0, 0, timedelta(0))
     cheapest_market = Market("", "", 0, 0.0, 0, 1_000_000_000, timedelta(0))
     for market in markets:
@@ -31,9 +38,9 @@ def main():
     target_market = cheapest_market
     if oldest_market.age > timedelta(days=7):
         target_market = oldest_market
-    profit_margin = (100 * (needed_most[1].payment - target_market.price) / needed_most[1].payment)
+    profit_margin = (100 * (commodity_needed_most.payment - target_market.price) / commodity_needed_most.payment)
     logger.info(f'Target market is "{target_market.station}" in the "{target_market.system}" system')
     logger.info(f"Profit margin: {profit_margin:.2f}%")
-    logger.info(f"Need {needed_most[1].needed} more {needed_most[0]}")
+    logger.info(f"Need {commodity_needed_most.needed} more {commodity_needed_most.name}")
     xerox.copy(target_market.system)
     logger.info("Target system name has been copied into clipboard")
